d1b2a3eb24fa8c97b926a6924ec28d5e7d8ac688
Prisha <prisha6thapar@gmail.com>
2022-12-20 18:19:34 +0530

    admin_page

---
+++ src/component/api.js
@@ -0,0 +1,27 @@
+export const questions = [
+    {
+        id: 1,
+        question: 'What is the Virtual Dom?',
+        answer: 'DOM stands for Document Object Model. The Dom represents an HTML document'
+    },
+    {
+        id: 2,
+        question: 'How do you create a React app?',
+        answer: 'Install the crreate-react-app package using the command prompt or terminal.'
+    },
+    {
+        id: 3,
+        question: 'What is an event in React?',
+        answer: 'An event is an action that a user or system may trigger, such as pressing a key'
+    },
+    {
+        id: 4,
+        question: 'What is synthetic events in React?',
+        answer: "Synthetic events combine the response of different browser's native events into"
+    },
+    {
+        id: 5,
+        question: 'What is components in React?',
+        answer: 'Components are the building blocks of any React application, and a single app using'
+    }
+]
---
+++ src/component/count_track.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './count_track.css';
+
+const Count = () => {
+    return (
+        <>
+        <p className='count'>Total file count : 6</p>
+        </>
+    )
+}
+
+export default Count;
